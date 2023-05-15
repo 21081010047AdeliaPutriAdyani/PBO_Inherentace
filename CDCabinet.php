9baa864695e83a12a72c5b6dc4995817c0b1bc90
21081010047AdeliaPutriAdyani <125734898+21081010047AdeliaPutriAdyani@users.noreply.github.com>
2023-05-15 14:10:13 +0700

    Add files via upload

---
+++ CDCabinet.php
@@ -0,0 +1,33 @@
+<?php
+    require_once('Product.php');
+    class CDCabinet extends Product{
+        protected $capacity;
+        protected $model;
+
+        function __construct($name, $capacity, $model, $price, $discount) {
+            parent::__construct($name, $price, $discount);
+            $this->capacity = $capacity;
+            $this->model = $model;
+        }
+
+        public function setPrice($price = 0){
+            parent::setPrice($price + (0.15 * $price));
+        }
+
+        public function getCapacity(){
+            return $this->capacity;
+        }
+
+        public function setCapacity($capacity){
+            $this->capacity=$capacity;
+        }
+
+        public function getModel(){
+            return $this->model;
+        }
+
+        public function setModel($model){
+            $this->model=$model;
+        }
+    }
+?>
